6576cfec7e535a26d38437cf3adbdf029a7597f9
rezrah <13340707+rezrah@users.noreply.github.com>
2021-12-28 16:20:32 +0000

    adjust react docs example

--- packages/documentation/pages/index.tsx
+++ packages/documentation/pages/index.tsx
@@ -3,42 +3,48 @@ import styles from "../styles/Home.module.css";
 import { Button } from "@rezrah/react";
 import buttonData from "@rezrah/design-tokens/dist/colours/components/button/js/light.module";
 
-type Variants = keyof typeof buttonData.btn;
-
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <div>
-          <h2 className={styles.title}>React buttons</h2>
-          <p className={styles.description}>Encapsulated styles</p>
+          <h2 className={styles.title}>React</h2>
+          <p className={styles.description}>Style encapsulation</p>
           {(
             Object.keys(buttonData.btn) as Array<keyof typeof buttonData.btn>
           ).map((value) => {
+            const buttonType = value.toString();
+            const capitalizedButtonType =
+              buttonType.charAt(0).toUpperCase() + buttonType.slice(1);
+
             return (
               <Button
-                key={value}
+                key={buttonType}
                 type={value !== "base" ? value : undefined}
-                onClick={() => console.log("wot")}
+                onClick={() => console.log("clicked!")}
               >
-                {value.charAt(0).toUpperCase() + value.slice(1)} Button
+                {capitalizedButtonType} Button
               </Button>
             );
           })}
         </div>
         <div>
-          <h2 className={styles.title}>CSS buttons</h2>
+          <h2 className={styles.title}>HTML + CSS</h2>
           <p className={styles.description}>Global styles</p>
           {(
             Object.keys(buttonData.btn) as Array<keyof typeof buttonData.btn>
-          ).map((type) => {
+          ).map((value) => {
+            const buttonType = value.toString();
+            const capitalizedButtonType =
+              buttonType.charAt(0).toUpperCase() + buttonType.slice(1);
+
             return (
               <button
-                key={type}
-                className={type !== "base" ? `btn btn-${type}` : `btn`}
-                onClick={() => console.log("wot")}
+                key={buttonType}
+                className={value !== "base" ? `btn btn-${buttonType}` : `btn`}
+                onClick={() => console.log("clicked!")}
               >
-                {type.charAt(0).toUpperCase() + type.slice(1)} Button
+                {capitalizedButtonType} Button
               </button>
             );
           })}
