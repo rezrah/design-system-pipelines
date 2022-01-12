3c0317fc5223cc59ca8934cf664053ee50ac1057
Reza Rahman <rezrah@github.com>
2022-01-12 15:13:41 +0000

    chore: cleanup rollup build file

--- packages/react/rollup.config.js
+++ packages/react/rollup.config.js
@@ -1,66 +1,6 @@
-// import babel from "rollup-plugin-babel";
-// import postcss from "rollup-plugin-postcss";
-// import resolve from "@rollup/plugin-node-resolve";
-//import commonjs from "@rollup/plugin-commonjs";
 import postcssImport from "postcss-import";
 import postcssEnv from "postcss-preset-env";
 import autoprefixer from "autoprefixer";
-
-// const extensions = [".js", ".jsx", ".ts", ".tsx"];
-
-// const globals = {
-//   react: "React",
-//   "react-dom": "ReactDOM",
-//   "core-js": "core-js",
-// };
-
-// const globalModules = Object.keys(globals);
-
-const sourceMap = true;
-
-// export default {
-//   input: {
-//     index: "./src/index.ts",
-//   },
-//   preserveModules: false,
-//   treeshake: false,
-//   output: [
-//     {
-//       dir: "dist",
-//       format: "esm",
-//       globals,
-//       sourcemap: sourceMap,
-//       preferConst: true,
-//     },
-//   ],
-//   plugins: [
-//     resolve({ extensions }),
-//     commonjs({
-//       include: "**/node_modules/**",
-//       namedExports: {},
-//     }),
-//     babel({
-//       sourceMap,
-//       extensions,
-//       include: ["src/**/*"],
-//       exclude: ["node_modules/**", "**/*.css"],
-//     }),
-//     postcss({
-//       plugins: [postcssImport(), postcssEnv(), autoprefixer()],
-//       namedExports: true,
-//       modules: {
-//         generateScopedName: "[folder]_[local]-[hash:base64:5]",
-//       },
-//       sourceMap,
-//       configFile: false,
-//       extract: true,
-//       preserveModules: true,
-//       writeDefinitions: true,
-//     }),
-//   ],
-//   external: (id) => globalModules.includes(id) || /core-js/.test(id),
-// };
-
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
@@ -71,6 +11,8 @@ import dts from "rollup-plugin-dts";
 import json from "@rollup/plugin-json";
 
 const packageJson = require("./package.json");
+
+const sourceMap = true;
 
 export default [
   {
